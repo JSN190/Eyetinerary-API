0a41b1dd91ebffd18d414ca3c3e0c86bb24fa495
John Su <30964725+JSN190@users.noreply.github.com>
2019-06-02 17:55:16 +0100

    Item module exports and controller

--- src/items/item.module.ts
+++ src/items/item.module.ts
@@ -2,13 +2,20 @@ import { Module } from '@nestjs/common';
 import { DatabaseModule } from '../database/database.module';
 import { itemProviders } from './item.providers';
 import { ItemService } from './item.service';
+import { PageModule } from '../pages/page.module';
+import { ItemController } from './item.controller';
 
 @Module({
-    imports: [DatabaseModule],
+    imports: [DatabaseModule, PageModule],
     providers: [
         ...itemProviders,
         ItemService,
     ],
+    exports: [
+        ...itemProviders,
+        ItemService,
+    ],
+    controllers: [ ItemController ],
 })
 
 export class ItemModule {}
