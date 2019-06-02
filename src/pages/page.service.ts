beff5aab432352ba790d5a67cba7d097f746fa31
John Su <30964725+JSN190@users.noreply.github.com>
2019-06-02 11:55:40 +0100

    findOne and createNew methods for PageService

--- src/pages/page.service.ts
+++ src/pages/page.service.ts
@@ -1,10 +1,24 @@
 import { Injectable, Inject } from '@nestjs/common';
 import { Repository } from 'typeorm';
 import { Page } from './page.entity';
+import { Itinerary } from '../itineraries/itinerary.entity';
 
 @Injectable()
 export class PageService {
     constructor(
         @Inject('PAGE_REPOSITORY') private readonly repository: Repository<Page>,
     ) {}
+
+    async findOne(id: number): Promise<Page> {
+        return await this.repository.findOne({ id });
+    }
+
+    async createNew(title: string, itinerary: Itinerary) {
+        const inserted = await this.repository
+        .createQueryBuilder()
+        .insert()
+        .values({ title, itinerary })
+        .execute();
+        return inserted.identifiers[0].id;
+    }
 }
