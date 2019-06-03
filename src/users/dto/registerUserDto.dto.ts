ee3bd650df28b5ee0f5d07cdcc1aade07a4c3736
John Su <30964725+JSN190@users.noreply.github.com>
2019-06-03 12:02:44 +0100

    Create regiserUser DTO

---
+++ src/users/dto/registerUserDto.dto.ts
@@ -0,0 +1,7 @@
+export class RegisterUserDto {
+    readonly username: string;
+    readonly password: string;
+    readonly email: string;
+    readonly location: string;
+    readonly countryCode: string;
+}
