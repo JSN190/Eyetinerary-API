5fc0ee1a74f517977a0d62c62aace6c1b56aa5f3
John Su <git@tyncture.dev>
2019-06-13 14:45:48 +0100

    Implement PATCH /page/:id

--- src/pages/page.controller.ts
+++ src/pages/page.controller.ts
@@ -1,5 +1,5 @@
 import { Controller, Get, Param, Body, NotFoundException, BadRequestException,
-    Post, Delete, UnauthorizedException, Req } from '@nestjs/common';
+    Post, Delete, UnauthorizedException, Req, Patch } from '@nestjs/common';
 import { PageService } from './page.service';
 import { Page } from './page.entity';
 import { ItineraryService } from '../itineraries/itinerary.service';
@@ -7,6 +7,7 @@ import { Itinerary } from '../itineraries/itinerary.entity';
 import { CreatePageDto } from './dto/createPage.dto';
 import { IntineraryAuth } from '../itineraries/itinerary.auth';
 import { DeletePageDto } from './dto/deletePageDto.dto';
+import { EditPageDto } from './dto/editPageDto.dto';
 
 @Controller('page')
 export class PageController {
@@ -42,6 +43,28 @@ export class PageController {
             throw new BadRequestException(`Itinerary ${body.itinerary} not found`,
             'Itinerary Not Found');
         }
+    }
+
+    @Patch(':id')
+    async editPage(@Param() params, @Body() body: EditPageDto, @Req() req) {
+        const page: Page = await this.pageService.findOne(params.id);
+        if (!page) {
+            throw new NotFoundException(`Page ${params.id} not found`, 'Page Not Found');
+        }
+
+        if (body.editToken) {
+            await this.itineraryAuth.verifyEditToken(body.editToken, page.itinerary);
+        } else if (req.token) {
+            await this.itineraryAuth.verifyOwnership(req.token, page.itinerary);
+        } else {
+            throw new UnauthorizedException('No Token Supplied', 'No Token Supplied');
+        }
+
+        const updated = await this.pageService.updateOne(page.id, body.title);
+        return {
+            success: true,
+            updated,
+        };
     }
 
     @Delete(':id')
