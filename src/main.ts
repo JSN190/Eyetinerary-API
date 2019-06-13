9a468729cb54b465760d8318f29832b58a6eb10c
John Su <30964725+JSN190@users.noreply.github.com>
2019-06-13 05:50:46 +0100

    Global validation pipe

--- src/main.ts
+++ src/main.ts
@@ -3,10 +3,12 @@ import {
   FastifyAdapter, NestFastifyApplication,
 } from '@nestjs/platform-fastify';
 import { AppModule } from './app.module';
+import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create<NestFastifyApplication>
     (AppModule, new FastifyAdapter(), { logger: console });
+  app.useGlobalPipes(new ValidationPipe());
   await app.listen(3000);
 }
 bootstrap();
