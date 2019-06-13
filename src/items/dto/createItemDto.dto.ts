38fc3467f93d7274f6d6ca73017809c4b5136cce
John Su <30964725+JSN190@users.noreply.github.com>
2019-06-13 05:51:42 +0100

    Validation for POST /item

--- src/items/dto/createItemDto.dto.ts
+++ src/items/dto/createItemDto.dto.ts
@@ -1,7 +1,22 @@
+import {IsAscii, IsNumber, IsPositive, IsDefined} from 'class-validator';
+import { Optional } from '@nestjs/common';
+
 export class CreateItemDto {
+    @IsDefined()
+    @IsAscii()
     readonly title: string;
+
+    @IsDefined()
+    @IsAscii()
     readonly body: string;
+
+    @IsDefined()
+    @IsPositive()
     readonly page: number;
+
+    @IsDefined()
     readonly timeStart: string | number;
+
+    @Optional()
     readonly timeEnd: string | number;
 }
