4c4a816035e2b511a1bae2898aec59aa70c58ffc
John Su <git@tyncture.dev>
2019-06-13 11:12:54 +0100

    Authentication for DELETE /item/:id

--- src/items/item.controller.ts
+++ src/items/item.controller.ts
@@ -1,15 +1,17 @@
-import { Controller, Get, Param, NotFoundException, Post, Body, BadRequestException, Delete } from '@nestjs/common';
+import { Controller, Get, Param, NotFoundException, Post, Body, BadRequestException, Delete, Req, UnauthorizedException } from '@nestjs/common';
 import { ItemService } from './item.service';
 import { Item } from './item.entity';
 import { PageService } from '../pages/page.service';
 import { Page } from '../pages/page.entity';
 import { CreateItemDto } from './dto/createItemDto.dto';
+import { IntineraryAuth } from '../itineraries/itinerary.auth';
 
 @Controller('item')
 export class ItemController {
     constructor(
         private readonly itemService: ItemService,
         private readonly pageService: PageService,
+        private readonly itineraryAuth: IntineraryAuth,
     ) {}
 
     @Get(':id')
@@ -45,15 +47,24 @@ export class ItemController {
     }
 
     @Delete(':id')
-    async deleteItem(@Param() params) {
-        const deleted: Item = await this.itemService.deleteOne(params.id);
-        if (deleted) {
-            return {
-                success: true,
-                deleted,
-            };
-        } else {
+    async deleteItem(@Param() params, @Body() body, @Req() req) {
+        const item: Item = await this.itemService.findOne(params.id);
+        if (!item) {
             throw new NotFoundException(`Item ${params.id} not found`, 'Item Not Found');
         }
+
+        if (body.editToken) {
+            await this.itineraryAuth.verifyEditToken(body.editToken, item.page.itinerary);
+        } else if (req.token) {
+            await this.itineraryAuth.verifyOwnership(req.token, item.page.itinerary);
+        } else {
+            throw new UnauthorizedException('No Token Supplied', 'No Token Supplied');
+        }
+
+        const deleted = await this.itemService.deleteOne(params.id);
+        return {
+            success: true,
+            deleted,
+        };
     }
 }
--- src/items/item.module.ts
+++ src/items/item.module.ts
@@ -4,9 +4,10 @@ import { itemProviders } from './item.providers';
 import { ItemService } from './item.service';
 import { PageModule } from '../pages/page.module';
 import { ItemController } from './item.controller';
+import { ItineraryModule } from '../itineraries/itinerary.module';
 
 @Module({
-    imports: [DatabaseModule, PageModule],
+    imports: [DatabaseModule, PageModule, ItineraryModule],
     providers: [
         ...itemProviders,
         ItemService,
