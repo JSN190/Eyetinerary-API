d39deb800202e0ffb471b0c0089e4910d5367bc0
John Su <30964725+JSN190@users.noreply.github.com>
2019-06-13 05:58:59 +0100

    Validation for POST /user

--- src/users/dto/registerUserDto.dto.ts
+++ src/users/dto/registerUserDto.dto.ts
@@ -1,7 +1,24 @@
+import { IsDefined, IsAlphanumeric, IsAscii, IsEmail, IsOptional, IsAlpha, Length } from 'class-validator';
+
 export class RegisterUserDto {
+    @IsDefined()
+    @IsAlphanumeric()
     readonly username: string;
+
+    @IsDefined()
+    @IsAscii()
     readonly password: string;
+
+    @IsOptional()
+    @IsEmail()
     readonly email: string;
+
+    @IsOptional()
+    @IsAscii()
     readonly location: string;
+
+    @IsOptional()
+    @IsAlpha()
+    @Length(2, 2)
     readonly countryCode: string;
 }
