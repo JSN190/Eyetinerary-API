01832e15a22ccb2345e6f93ba588adb01e1546a1
John Su <30964725+JSN190@users.noreply.github.com>
2019-06-13 05:54:33 +0100

    Validation for POST /itinerary

--- src/itineraries/dto/createItineraryDto.dto.ts
+++ src/itineraries/dto/createItineraryDto.dto.ts
@@ -1,4 +1,10 @@
+import { IsDefined, IsAscii, IsPositive } from "class-validator";
+
 export class CreateItineraryDto {
+    @IsDefined()
+    @IsAscii()
     readonly title: string;
+
+    @IsPositive()
     readonly owner: number;
 }
