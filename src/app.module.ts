c6beb636e9cab9ef048ec2fc91a354b312861aff
John Su <30964725+JSN190@users.noreply.github.com>
2019-06-13 08:11:04 +0100

    HTTP bearer token middleware

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,14 +1,23 @@
-import { Module } from '@nestjs/common';
+import { Module, MiddlewareConsumer, RequestMethod } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { ItemModule } from './items/item.module';
 import { PageModule } from './pages/page.module';
 import { ItineraryModule } from './itineraries/itinerary.module';
 import { AuthModule } from './auth/auth.module';
+import { ExpressBearerTokenMiddleware } from '@nest-middlewares/express-bearer-token';
 
 @Module({
   imports: [ItemModule, PageModule, ItineraryModule, AuthModule],
   controllers: [AppController],
   providers: [AppService],
 })
-export class AppModule {}
+export class AppModule {
+  configure(consumer: MiddlewareConsumer): void {
+    consumer.apply(ExpressBearerTokenMiddleware)
+    .forRoutes({
+      path: '*',
+      method: RequestMethod.ALL,
+    });
+  }
+}
