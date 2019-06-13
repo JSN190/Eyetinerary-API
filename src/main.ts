cf330546af55747bc4660eb3be648004f994b4e5
John Su <30964725+JSN190@users.noreply.github.com>
2019-06-13 06:11:26 +0100

    Switch to Express

--- src/main.ts
+++ src/main.ts
@@ -1,13 +1,9 @@
 import { NestFactory } from '@nestjs/core';
-import {
-  FastifyAdapter, NestFastifyApplication,
-} from '@nestjs/platform-fastify';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
-  const app = await NestFactory.create<NestFastifyApplication>
-    (AppModule, new FastifyAdapter(), { logger: console });
+  const app = await NestFactory.create(AppModule, { logger: console });
   app.useGlobalPipes(new ValidationPipe());
   await app.listen(3000);
 }
